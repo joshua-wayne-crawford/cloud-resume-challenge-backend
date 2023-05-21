6623d130c76de5cd813e9963358be20b3976ebbb
Joshua Crawford <joshuawaynecrawford@gmail.com>
2023-05-20 23:46:13 -0500

    added test cleanup

--- Testing/AzFunctionTest.cs
+++ Testing/AzFunctionTest.cs
@@ -46,5 +46,34 @@ namespace Resume
 
             Assert.IsTrue(resultBeforeFunctionCall < resultAfterFunctionCall);
         }
+
+        [TestCleanup]
+        public static void CleanUp()
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("local.settings.json", true, true).AddEnvironmentVariables().Build();
+            Environment.SetEnvironmentVariable("connection_string", config["Values:connection_string"]);
+            string tableName = "views";
+            connString = Environment.GetEnvironmentVariable("connection_string",EnvironmentVariableTarget.Process);
+            int views = 0;
+
+            TableClient tableClient = new TableClient(connString, tableName);
+
+            Pageable<TableEntity> results = tableClient.Query<TableEntity>(entity => entity.PartitionKey == "views");
+
+            foreach (TableEntity tableEntity in results)
+            {
+                views = tableEntity.views;
+            }
+
+            //remove the view count added by the test
+            TableEntity entity = new TableEntity()
+            {
+                PartitionKey = "views",
+                RowKey = "0",
+                views = views-1
+            };
+
+            tableClient.UpsertEntity(entity);
+        }
     }
 }
